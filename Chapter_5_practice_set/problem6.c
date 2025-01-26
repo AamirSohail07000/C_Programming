41cbdb845a4bc2d879a2e51dc029fd7416f3a749
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-01-26 23:10:28 +0530

    solved chapter 5 practice set

---
+++ Chapter_5_practice_set/problem6.c
@@ -0,0 +1,1 @@
+// 6. Write a recursive function to calculate the sum of first ‘n’ natural numbers.
---
+++ Chapter_5_practice_set/problem7.c
@@ -0,0 +1,4 @@
+// 7. Write a program using function to print the following pattern (first n lines)
+// *
+// * * *
+// * * * * *
